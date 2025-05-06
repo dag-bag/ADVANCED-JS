d384d6519c7c75635d246f4edefa1d97569a05c3
dag-bag <88625252+dag-bag@users.noreply.github.com>
2025-05-06 23:12:13 +0530

    + DONE

---
+++ Obj.js
@@ -0,0 +1,15 @@
+// let obj = {
+//   name: "virender",
+//   address: "RZC2-110   ",
+// };
+// Object.freeze(obj);
+
+// obj.name = "Rahul Kumar";
+// console.log(obj);
+
+const set = new Set([1, 2, 3]);
+
+for (const value in set) {
+  console.log(value); // 1, 2, 3
+}
+let str = "virender";
---
+++ class.js
@@ -0,0 +1,44 @@
+// class Person {
+//   static age = 16;
+//   name = "virender";
+//   cast = "Kumar";
+//   value;
+//   constructor(value) {
+//     this.value = value;
+//   }
+//   talking(who) {
+//     return who + " Talking";
+//   }
+// }
+// Person.prototype.walk = function () {
+//   return "i can walk";
+// };
+// const me = new Person("Virender");
+// me.fly = function () {
+//   return "i can fly";
+// };
+// // me.talking("me");
+// // const you = new Person();
+// // you.talking("you");
+// // me.age = 10;
+// // console.log(me);
+// console.log(me.__proto__);
+// console.log(Person.prototype);
+
+class Person {
+  walk() {
+    return "I can walk.";
+  }
+}
+class SuperHuman extends Person {
+  fly() {
+    return "i can fly";
+  }
+}
+const me = new SuperHuman();
+
+me.__proto__.fight = function () {
+  return "I Can Fight";
+};
+const you = new SuperHuman();
+console.log(you.fight());
