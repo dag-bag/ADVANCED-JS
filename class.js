27f40f471895595d0b3c4f5b7c6a9b97e78db023
dag-bag <88625252+dag-bag@users.noreply.github.com>
2025-05-08 01:07:28 +0530

    Update class.js

--- class.js
+++ class.js
@@ -1,44 +1,46 @@
+// // class Person {
+// //   static age = 16;
+// //   name = "virender";
+// //   cast = "Kumar";
+// //   value;
+// //   constructor(value) {
+// //     this.value = value;
+// //   }
+// //   talking(who) {
+// //     return who + " Talking";
+// //   }
+// // }
+// // Person.prototype.walk = function () {
+// //   return "i can walk";
+// // };
+// // const me = new Person("Virender");
+// // me.fly = function () {
+// //   return "i can fly";
+// // };
+// // // me.talking("me");
+// // // const you = new Person();
+// // // you.talking("you");
+// // // me.age = 10;
+// // // console.log(me);
+// // console.log(me.__proto__);
+// // console.log(Person.prototype);
+
 // class Person {
-//   static age = 16;
-//   name = "virender";
-//   cast = "Kumar";
-//   value;
-//   constructor(value) {
-//     this.value = value;
-//   }
-//   talking(who) {
-//     return who + " Talking";
+//   walk() {
+//     return "I can walk.";
 //   }
 // }
-// Person.prototype.walk = function () {
-//   return "i can walk";
-// };
-// const me = new Person("Virender");
-// me.fly = function () {
-//   return "i can fly";
-// };
-// // me.talking("me");
-// // const you = new Person();
-// // you.talking("you");
-// // me.age = 10;
-// // console.log(me);
-// console.log(me.__proto__);
-// console.log(Person.prototype);
+// class SuperHuman extends Person {
+//   fly() {
+//     return "i can fly";
+//   }
+// }
+// const me = new SuperHuman();
 
-class Person {
-  walk() {
-    return "I can walk.";
-  }
-}
-class SuperHuman extends Person {
-  fly() {
-    return "i can fly";
-  }
-}
-const me = new SuperHuman();
+// me.__proto__.fight = function () {
+//   return "I Can Fight";
+// };
+// const you = new SuperHuman();
+// console.log(you.fight());
 
-me.__proto__.fight = function () {
-  return "I Can Fight";
-};
-const you = new SuperHuman();
-console.log(you.fight());
+console.dir(Number);
