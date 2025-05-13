5027ce5395176ce31c0ce9979642961a997ce900
dag-bag <88625252+dag-bag@users.noreply.github.com>
2025-05-13 22:59:36 +0530

    ++ init

--- class.js
+++ class.js
@@ -49,23 +49,37 @@
 // name();
 // console.dir(name);
 
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
-// Fake JSON API
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+// // Fake JSON API
+// const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-xhr.open("GET", url, true);
+// xhr.open("GET", url, true);
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    // 4 = DONE
-    if (xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-      console.log("Fake JSON data:", response);
-    } else {
-      console.error("Failed to fetch data");
-    }
-  }
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     // 4 = DONE
+//     if (xhr.status === 200) {
+//       const response = JSON.parse(xhr.responseText);
+//       console.log("Fake JSON data:", response);
+//     } else {
+//       console.error("Failed to fetch data");
+//     }
+//   }
+// };
+
+// xhr.send();
+
+const Outer = () => {
+  console.log("I'm Outer");
 };
+const Mid = () => {
+  console.log("I'm Mid");
+};
+const Inner = (e) => {
+  e.stopPropagation();
+  console.log("I'm Inner");
+};
+// const name2 = () => {};
 
-xhr.send();
+// console.log(a);
